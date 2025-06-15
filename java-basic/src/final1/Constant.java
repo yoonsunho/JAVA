fa2b624c57a3f370ad3ffc0e39ac91121cefa361
yoonsunho <y_sunho@naver.com>
2025-06-15 19:34:29 +0900

    final

---
+++ java-basic/src/final1/Constant.java
@@ -0,0 +1,7 @@
+package final1;
+
+public class Constant {
+
+    // 수학 상수
+    public static final double PI = 3.14;
+}
---
+++ java-basic/src/final1/FieldInit.java
@@ -0,0 +1,9 @@
+package final1;
+
+public class FieldInit {
+
+    //static final - 메모리 중복을 막아줌
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+
+}
