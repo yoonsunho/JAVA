81ee51152e8461cfed75f87935d62e41a4ebdc10
yoonsunho <y_sunho@naver.com>
2025-07-19 02:05:40 +0900

    mvc

---
+++ hello-spring/src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package src.main.java.hello.hello_spring.repository;
+
+import src.main.java.hello.hello_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,33 @@
+package src.main.java.hello.hello_spring.repository;
+
+import src.main.java.hello.hello_spring.domain.Member;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return List.of();
+    }
+}
