72cfc19e485c189ffb3250e1eba39ff9ccfaa4f6
yoonsunho <y_sunho@naver.com>
2025-07-26 23:06:18 +0900

    servlet/response

--- servlet/src/main/java/hello/servlet/basic/HelloData.java
+++ servlet/src/main/java/hello/servlet/basic/HelloData.java
@@ -1,8 +1,15 @@
 package hello.servlet.basic;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class HelloData {
 
     private String username;
     private int age;
+
+
 
 }
